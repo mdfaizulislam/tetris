1207e58546945e88e023ed37750f811cc06e0d13
Md Faizul Islam <36614551+mdfaizulislam@users.noreply.github.com>
2023-05-18 13:46:15 +0600

    added play game popup

---
+++ assets/Scripts/Components/popups/PlayGamePopupComponent.ts
@@ -0,0 +1,44 @@
+/**
+ * Title: Play Popup
+ * Description: Enter into game from this popup
+ * Author: Md. Faizul Islam (faizul7cse@gmail.com)
+ * Date: 18-05-2023 13:44
+ */
+
+import { Constants } from '../../Constants';
+import { Helper } from '../../Generic/Helper';
+import PersistentComponent from '../PersistentComponent';
+import PopupComponent from './PopupComponent';
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class PlayGamePopupComponent extends cc.Component {
+    @property(cc.Button)
+    buttonPlay: cc.Button = null;
+
+    private mPersistentComponent: PersistentComponent = null;
+
+    protected onLoad(): void {
+        this.mPersistentComponent = cc.find('PersistantNode').getComponent(PersistentComponent);
+        this.buttonPlay.clickEvents.push(
+            Helper.getEventHandler(this.node, 'PlayGamePopupComponent', 'OnPlayPressed')
+        );
+        this.setPlayButtonInteractibility(false);
+    }
+
+    public initialize(): void {
+        this.setPlayButtonInteractibility(true);
+    }
+
+    OnPlayPressed() {
+        console.log('OnPlayAgainPressed');
+        this.setPlayButtonInteractibility(false);
+        this.mPersistentComponent.node.emit(Constants.EVENTS.PLAY_TETRIS);
+        this.getComponent(PopupComponent).hidePopup();
+    }
+
+    setPlayButtonInteractibility(isInteractible: boolean): void {
+        this.buttonPlay.interactable = isInteractible;
+    }
+}
